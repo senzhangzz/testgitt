6da349468ab958ee3d0e8ac8a8eea0d033043ad7
张森 <864486270@qq.com>
2017-10-11 17:04:04 +0800

    no message

--- testgitt/test.m
+++ testgitt/test.m
@@ -10,12 +10,15 @@
 
 @implementation test
 
-/*
+
+
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    NSLog(@"1");
 }
-*/
+
 
 @end
