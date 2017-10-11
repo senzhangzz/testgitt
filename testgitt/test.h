9f8322f9da509c659bd3b3fe1248722dc2f7c9c6
张森 <864486270@qq.com>
2017-10-11 17:02:54 +0800

    no message

---
+++ testgitt/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  testgitt
+//
+//  Created by 张森 on 2017/10/11.
+//  Copyright © 2017年 ZhangSen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ testgitt/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  testgitt
+//
+//  Created by 张森 on 2017/10/11.
+//  Copyright © 2017年 ZhangSen. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
